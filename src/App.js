97c7deb8423cde38e3c44c872fcf2a094e8c9f97
olivercastroteran <olivercastroteran@gmail.com>
2020-09-04 10:48:49 -0400

    searchField and amount select added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import NavBar from './components/navbar/NavBar';
+import Search from './components/search/Search';
 
 function App() {
   return (
     <MuiThemeProvider>
       <div>
         <NavBar />
+        <Search />
       </div>
     </MuiThemeProvider>
   );
---
+++ src/components/search/Search.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import { API_Key } from '../../pixabay/pixabay';
+
+class Search extends Component {
+  state = {
+    searchText: '',
+    amount: 15,
+    apiUrl: 'https://pixabay.com/api',
+    apiKey: API_Key,
+    images: [],
+  };
+
+  onTextChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onAmountChange = (e) => {};
+
+  render() {
+    return (
+      <div>
+        <TextField
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          floatingLabelText="Search For Images"
+          fullWidth={true}
+        />
+        <br />
+        <SelectField
+          name="amount"
+          floatingLabelText="Amount"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        >
+          <MenuItem value={5} primaryText="5" />
+          <MenuItem value={10} primaryText="10" />
+          <MenuItem value={15} primaryText="15" />
+          <MenuItem value={30} primaryText="30" />
+          <MenuItem value={50} primaryText="50" />
+        </SelectField>
+        <br />
+      </div>
+    );
+  }
+}
+
+export default Search;
